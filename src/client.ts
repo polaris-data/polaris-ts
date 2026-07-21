934f1237a0c9b99ef32df38e59b929bbde840ce9
Hilliam T. <hilliamtung@gmail.com>
2026-07-21 19:50:06 +0100

    Bump version to 0.5.2 (#26)

--- src/client.ts
+++ src/client.ts
@@ -58,7 +58,7 @@ import type { PolarisRuntime } from "./runtime/types";
 // SDK version – bumped manually during releases
 // ---------------------------------------------------------------------------
 
-const VERSION = "0.5.1";
+const VERSION = "0.5.2";
 
 // ---------------------------------------------------------------------------
 // Internal shorthand
